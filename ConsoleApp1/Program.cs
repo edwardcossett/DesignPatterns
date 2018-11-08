4d68bb4af975d8402ce5802590b3d0529bf2eaf7
Edward Cossett <edward.cossett@gmail.com>
2018-11-08 01:04:17 -0600

    Code is working. Will modify for my needs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace CommandDesign
 {
-    class Program
+    class CommandApp
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Create receiver, command, and invoker
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            // Set and execute command
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Command
+    {
+        protected Receiver receiver;
+        // Constructor
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+        public abstract void Execute();
+    }
+
+    class ConcreteCommand : Command
+    {
+        // Constructor
+        public ConcreteCommand(Receiver receiver) :
+          base(receiver)
+        {
+        }
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+
+    class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+
+    class Invoker
+    {
+        private Command _command;
+        public void SetCommand(Command command)
+        {
+            this._command = command;
+        }
+        public void ExecuteCommand()
+        {
+            _command.Execute();
         }
     }
 }
+
+
+
